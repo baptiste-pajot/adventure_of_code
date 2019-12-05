ac6e61ee60032f4f6f60ce697ecb3b49733788bb
baptiste-pajot <bpajot@student.42.fr>
2019-12-05 15:41:08 +0100

    day05_1

---
+++ day_05/day05_1.py
@@ -0,0 +1,39 @@
+import math
+
+def main():
+    f = open("input.txt", "r")
+    for x in f:
+        tab = list(map(int, x.split(",")))
+    i = 0
+    while 1:
+        if tab[i] == 99:
+            break
+        elif tab[i] % 10 == 1:
+            param1 =  tab[i + 1] if (tab[i] % 1000 // 100) else tab[tab[i + 1]]
+            param2 =  tab[i + 2] if (tab[i] % 10000 // 1000) else tab[tab[i + 2]]
+            if tab[i] // 10000:
+                tab[i + 3] = param1 + param2
+            else:
+                tab[tab[i + 3]] = param1 + param2
+            i += 4
+        elif tab[i] % 10 == 2:
+            param1 =  tab[i + 1] if (tab[i] % 1000 // 100) else tab[tab[i + 1]]
+            param2 =  tab[i + 2] if (tab[i] % 10000 // 1000) else tab[tab[i + 2]]
+            if tab[i] // 10000:
+                tab[i + 3] = param1 * param2
+            else :
+                tab[tab[i + 3]] = param1 * param2
+            i += 4
+        elif tab[i] % 10 == 3:
+            tab[tab[i + 1]] = int(input("value ?"))
+            i += 2
+        elif tab[i] % 10 == 4:
+            print(tab[tab[i + 1]])
+            i += 2
+        #print(i)
+        #print(tab)
+    #print(tab)
+
+
+if __name__ == "__main__":
+    main()
