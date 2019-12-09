79165cc240ad79fbf32abe44626d7a6ecbb6104c
baptiste-pajot <bpajot@student.42.fr>
2019-12-09 14:09:03 +0100

    day08_2.py

--- day_08/day08_2.py
+++ day_08/day08_2.py
@@ -8,12 +8,31 @@ def main():
     res = []
     for i in range(len(tab) // (width * height)):
         res.append([])
-    for i in range(len(tab)):
+    for i in range(0, len(tab), width):
         page = i // (width * height)
-        print(page)
-        res[page].append(tab[i])
+        res[page].append(tab[i: i + width])
+    end = []
+    for i in range(height):
+        end.append([])
+        for j in range(width):
+            val = 2
+            for page in range(len(tab) // (width * height)):
+                if res[page][i][j] != '2':
+                    val = res[page][i][j]
+                    end[i].append(int(val))
+                    break
     for i in range(len(tab) // (width * height)):
-        print(res[i])
+        print()
+        for j in range(height):
+            print(res[i][j])
+    print("")
+    for i in range(height):
+        for j in range(width):
+            if end[i][j] == 1:
+                print('O', end ='')
+            else:
+                print(' ', end ='')
+        print("")
 
 if __name__ == "__main__":
     main()
